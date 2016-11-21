c72429b039955c4e2e53adc522ee590d23430f7e
Dave Lively <davelively14@gmail.com>
2016-11-21 14:22:15 -0500

    Added HTTPoison and Floki to dependencies

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule SeoHero.Mixfile do
   def application do
     [mod: {SeoHero, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :httpoison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,6 +37,8 @@ defmodule SeoHero.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:httpoison, "~> 0.10.0"},
+     {:floki, "~> 0.11.0"},
      {:cowboy, "~> 1.0"}]
   end
 
