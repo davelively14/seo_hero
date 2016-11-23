402b9be99985b8947d5cafb389370e2e10137522
Dave Lively <davelively14@gmail.com>
2016-11-22 22:31:58 -0500

    Added SeoHero.Supervisor

---
+++ lib/seo_hero/supervisor.ex
@@ -0,0 +1,28 @@
+defmodule SeoHero.Supervisor do
+  use Supervisor
+
+  #######
+  # API #
+  #######
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, %{})
+  end
+
+  #############
+  # Callbacks #
+  #############
+
+  def init do
+    children = [
+      worker(SeoHero.Server, [])
+    ]
+
+    options = [
+      strategy: :one_for_one,
+      shutdown: 7_000
+    ]
+
+    supervise(children, options)
+  end
+end
