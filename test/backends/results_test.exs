56e77559e8e8dad05bc7ff4f31eb85abd7302cbb
Dave Lively <davelively14@gmail.com>
2016-11-22 19:16:55 -0500

    Added test to ensure no nil results

--- test/backends/results_test.exs
+++ test/backends/results_test.exs
@@ -2,10 +2,23 @@ defmodule SeoHero.ResultsTest do
   use SeoHero.ConnCase
   alias SeoHero.Results
 
+  setup config do
+    cond do
+      config[:fetch_data] ->
+        results = Results.fetch_data
+        {:ok, results: results}
+    end
+  end
+
   @tag :external
-  test "fetch_data/0 returns 9 results" do
-    results = Results.fetch_data
+  @tag :fetch_data
+  test "fetch_data/0 returns 9 results", %{results: results} do
     assert results |> length == 9
-    # assert results |> List.first |> String.length > 0
+  end
+
+  @tag :external
+  @tag :fetch_data
+  test "each result from fetch_data/0 contains data", %{results: results} do
+    assert results |> Enum.each(&(assert &1.domain != nil))
   end
 end
