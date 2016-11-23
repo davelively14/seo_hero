11e73d5f80ff927fbcd16ec1ec17b703bc2912f8
Dave Lively <davelively14@gmail.com>
2016-11-23 18:42:03 -0500

    Updated

--- priv/static/js/application.js
+++ priv/static/js/application.js
@@ -51,8 +51,23 @@
 /* 1 */
 /***/ function(module, exports) {
 
-	// Intentionally blank
-	"use strict";
+	// Although ^=parent is not technically correct,
+	// we need to use it in order to get IE8 support.
+	'use strict';
+
+	var elements = document.querySelectorAll('[data-submit^=parent]');
+	var len = elements.length;
+
+	for (var i = 0; i < len; ++i) {
+	  elements[i].addEventListener('click', function (event) {
+	    var message = this.getAttribute("data-confirm");
+	    if (message === null || confirm(message)) {
+	      this.parentNode.submit();
+	    };
+	    event.preventDefault();
+	    return false;
+	  }, false);
+	};
 
 /***/ }
 /******/ ]);
