b8be8c58eb85e17fb4eddacd93015a672c78fa2f
Dave Lively <davelively14@gmail.com>
2016-11-23 18:42:28 -0500

    Added delete function

--- web/controllers/results_controller.ex
+++ web/controllers/results_controller.ex
@@ -10,4 +10,15 @@ defmodule SeoHero.ResultsController do
       |> Repo.preload(:results)
     render conn, "index.html", collections: collections
   end
+
+  def delete(conn, %{"id" => id}) do
+    collection = ResultCollection |> Repo.get(id) |> Repo.preload(:results)
+
+    collection.results
+    |> Enum.each(&Repo.delete(&1))
+
+    collection |> Repo.delete
+
+    redirect(conn, to: results_path(conn, :index))
+  end
 end
