b5b5cda3dbfe6ae373a34c34b84cb9d2e43880b4
Dave Lively <davelively14@gmail.com>
2016-11-22 22:43:43 -0500

    Named SeoHero.Server to the module name

--- lib/seo_hero/server.ex
+++ lib/seo_hero/server.ex
@@ -9,7 +9,7 @@ defmodule SeoHero.Server do
   #######
 
   def start_link do
-    GenServer.start_link(__MODULE__, %{})
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
   #############
