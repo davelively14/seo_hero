fb8693998f1e5833b19e4465e23dda79bdfa6664
Dave Lively <davelively14@gmail.com>
2016-11-23 20:12:50 -0500

    Added readable_date

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,8 @@
 defmodule SeoHero.PageView do
   use SeoHero.Web, :view
+
+  def readable_date(dtg) do
+    {_, {{year, month, day}, {hour, minute, second, _}}} = Ecto.DateTime.dump(dtg)
+    "#{month}/#{day}/#{year} at #{hour}:#{minute}"
+  end
 end
