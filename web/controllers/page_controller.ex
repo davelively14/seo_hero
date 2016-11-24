499a5597e3f047a94bf43f3ec6097d71d82ad239
Dave Lively <davelively14@gmail.com>
2016-11-23 20:11:13 -0500

    Passes most recent collection via Server.get_state to view

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,8 @@
 defmodule SeoHero.PageController do
   use SeoHero.Web, :controller
+  alias SeoHero.Server
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", collection: Server.get_state
   end
 end
