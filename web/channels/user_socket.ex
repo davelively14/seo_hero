5e1dbe0963ffb3799aaf1bf231438c1f62633ad9
Dave Lively <davelively14@gmail.com>
2016-11-23 21:25:42 -0500

    Heroku settings implemented

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule SeoHero.UserSocket do
   # channel "room:*", SeoHero.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
