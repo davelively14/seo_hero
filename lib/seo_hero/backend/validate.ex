16f33d27ccb1c5148f3ed7a1d0596f890cae64a4
Dave Lively <davelively14@gmail.com>
2016-12-05 18:27:44 -0500

    ADD: ninja scraping capability

--- lib/seo_hero/backend/validate.ex
+++ lib/seo_hero/backend/validate.ex
@@ -17,16 +17,27 @@ defmodule SeoHero.Validate do
   ####################
 
   def get_url(domain) do
-    "https://www.whois.com/whois/#{domain}"
+    decon_url = domain |> String.split(".")
+    clean_domain = "#{Enum.at(decon_url, length(decon_url) - 2)}.#{List.last(decon_url)}"
+    "https://www.whois.com/whois/#{clean_domain}"
   end
 
   def get_creation_date(url) do
+    IO.inspect url
     case url |> String.split(".") |> List.last do
       "io" ->
         get_io(url)
+      "ninja" ->
+        get_ninja(url)
       _ ->
         get_standard(url)
     end
+  end
+
+  defp get_ninja(url) do
+    HTTPoison.get!(url)
+    |> Map.get(:body) |> String.split("Creation Date: ") |> Enum.at(1)
+    |> String.split("T") |> List.first |> make_date_io
   end
 
   defp get_io(url) do
@@ -38,7 +49,7 @@ defmodule SeoHero.Validate do
   defp get_standard(url) do
     HTTPoison.get!(url)
     |> Map.get(:body) |> String.split("Creation Date: ") |> Enum.at(1)
-    |> String.split("<br>") |> List.first |> make_date
+    |> String.split("<br>") |> List.first |> make_data_standard
   end
 
   def make_date_io(date) do
@@ -46,13 +57,10 @@ defmodule SeoHero.Validate do
 
     year = String.to_integer(year) - 1
 
-    result = {{year, String.to_integer(month), String.to_integer(day)}, {0, 0, 0}}
-    IO.inspect result
-    IO.inspect @contest_start_date
-    result
+    {{year, String.to_integer(month), String.to_integer(day)}, {0, 0, 0}}
   end
 
-  def make_date(date) do
+  def make_data_standard(date) do
     [day, month, year] = date |> String.split("-")
     month =
       case month do
@@ -71,10 +79,7 @@ defmodule SeoHero.Validate do
         _ -> String.to_integer(month)
       end
 
-    result = {{String.to_integer(year), month, String.to_integer(day)}, {0, 0, 0}}
-    IO.inspect result
-    IO.inspect @contest_start_date
-    result
+    {{String.to_integer(year), month, String.to_integer(day)}, {0, 0, 0}}
   end
 
   def is_eligible(result_date), do: result_date >= @contest_start_date
